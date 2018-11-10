0da28acc3b0190653ce51a633c9de0de28169d22
Heidi <heidi.hassinen@helsinki.fi>
2018-11-11 01:59:55 +0200

    frontend mukaan

---
+++ build/precache-manifest.27eb9664ae248d8131aed9a357596814.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d3130a5361e30ca503f7",
+    "url": "/static/js/main.d3130a53.chunk.js"
+  },
+  {
+    "revision": "b61b8ab4bcf6216c00b0",
+    "url": "/static/js/1.b61b8ab4.chunk.js"
+  },
+  {
+    "revision": "d3130a5361e30ca503f7",
+    "url": "/static/css/main.38626292.chunk.css"
+  },
+  {
+    "revision": "a8366b9c1a5efa73ba306d48cc73dcba",
+    "url": "/index.html"
+  }
+];
